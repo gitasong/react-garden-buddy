62dc163f405ba93827ad4722ce1a7dba43bfef68
Nicole Freed <nicole.freed@gmail.com>
2017-09-15 11:57:21 -0700

    add masterPlantList to PropTypes

--- src/components/PlantList.jsx
+++ src/components/PlantList.jsx
@@ -2,36 +2,36 @@ import React from "react";
 import Plant from "./Plant";
 import PropTypes from "prop-types";
 
-var masterPlantList = [
-    {
-      name: "English Ivy",
-      image: "http://www.guide-to-houseplants.com/images/xenglish-ivy-indoors.jpg.pagespeed.ic.wzOZdzWgZ1.jpg",
-      species: "Hedera helix",
-      category: "vine",
-      frequency: "1"
-    },
-    {
-      name: "Mother-In-Law's Tongue/Snake Plant",
-      image: "http://www.guide-to-houseplants.com/images/xsnake-plant.jpg.pagespeed.ic.2rLHSiwnxf.jpg",
-      species: "Sansevieria trifasciata",
-      category: "succulent",
-      frequency: "7"
-    },
-    {
-      name: "Spider Plant",
-      image: "http://www.guide-to-houseplants.com/images/xspider-plant-table.jpg.pagespeed.ic.BFy65MnY4p.jpg",
-      species: "Chlorophytum comosum",
-      category: "hanging",
-      frequency: "1"
-    },
-    {
-      name: "Weeping Fig (ficus)",
-      image: "http://www.guide-to-houseplants.com/images/xweeping-fig.jpg.pagespeed.ic.lqiATUUmqn.jpg",
-      species: "Ficus benjamina",
-      category: "indoor tree",
-      frequency: "3"
-    }
-  ];
+// var masterPlantList = [
+//     {
+//       name: "English Ivy",
+//       image: "http://www.guide-to-houseplants.com/images/xenglish-ivy-indoors.jpg.pagespeed.ic.wzOZdzWgZ1.jpg",
+//       species: "Hedera helix",
+//       category: "vine",
+//       frequency: "1"
+//     },
+//     {
+//       name: "Mother-In-Law's Tongue/Snake Plant",
+//       image: "http://www.guide-to-houseplants.com/images/xsnake-plant.jpg.pagespeed.ic.2rLHSiwnxf.jpg",
+//       species: "Sansevieria trifasciata",
+//       category: "succulent",
+//       frequency: "7"
+//     },
+//     {
+//       name: "Spider Plant",
+//       image: "http://www.guide-to-houseplants.com/images/xspider-plant-table.jpg.pagespeed.ic.BFy65MnY4p.jpg",
+//       species: "Chlorophytum comosum",
+//       category: "hanging",
+//       frequency: "1"
+//     },
+//     {
+//       name: "Weeping Fig (ficus)",
+//       image: "http://www.guide-to-houseplants.com/images/xweeping-fig.jpg.pagespeed.ic.lqiATUUmqn.jpg",
+//       species: "Ficus benjamina",
+//       category: "indoor tree",
+//       frequency: "3"
+//     }
+//   ];
 
 function PlantList(props){
   return (
@@ -51,7 +51,7 @@ function PlantList(props){
 }
 
 PlantList.propTypes = {
-  plantList: PropTypes.array
+  masterPlantList: PropTypes.array
 };
 
 export default PlantList;
