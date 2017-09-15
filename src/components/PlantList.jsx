c7573e5e3569d289b849fc13ba2d39ea6a0952be
Nicole Freed <nicole.freed@gmail.com>
2017-09-15 09:45:15 -0700

    create masterPlantList

--- src/components/PlantList.jsx
+++ src/components/PlantList.jsx
@@ -2,18 +2,49 @@ import React from "react";
 import Plant from "./Plant";
 import PropTypes from "prop-types";
 
+var masterPlantList = [
+    {
+      name: "English Ivy",
+      image: "http://www.guide-to-houseplants.com/images/xenglish-ivy-indoors.jpg.pagespeed.ic.wzOZdzWgZ1.jpg",
+      species: "Hedera helix",
+      category: "vine",
+      frequency: "1"
+    },
+    {
+      name: "Mother-In-Law's Tongue/Snake Plant",
+      image: "http://www.guide-to-houseplants.com/images/xsnake-plant.jpg.pagespeed.ic.2rLHSiwnxf.jpg",
+      species: "Sansevieria trifasciata",
+      category: "succulent",
+      frequency: "7"
+    },
+    {
+      name: "Spider Plant",
+      image: "http://www.guide-to-houseplants.com/images/xspider-plant-table.jpg.pagespeed.ic.BFy65MnY4p.jpg",
+      species: "Chlorophytum comosum",
+      category: "hanging",
+      frequency: "1"
+    },
+    {
+      name: "Weeping Fig (ficus)",
+      image: "http://www.guide-to-houseplants.com/images/xweeping-fig.jpg.pagespeed.ic.lqiATUUmqn.jpg",
+      species: "Ficus benjamina",
+      category: "indoor tree",
+      frequency: "3"
+    }
+  ];
+
 function PlantList(props){
   return (
     <div>
       <hr/>
-      {props.plantList.map((plant, index) =>
+    {props.masterPlantList.map((plant, index) =>
         <Plant name={plant.name}
         image={plant.image}
         species={plant.species}
         category={plant.category}
         frequency={plant.frequency}
         timeSinceWatered={plant.timeSinceWatered}
-        key={index}/>
+        />
       )}
     </div>
   );
