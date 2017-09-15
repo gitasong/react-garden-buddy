c1347701dc42ea0b6404ea1968b4e4fcdc58ba66
Nicole Freed <nicole.freed@gmail.com>
2017-09-15 09:52:51 -0700

    import and add Header component to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "./Header";
 import PlantList from "./PlantList";
 
 function App(props){
   return (
     <div>
+      <Header/>
       <PlantList/>
     </div>
   );
