40f78f983742c02e3f98d96f9082c4490e5b3278
Nicole Freed <nicole.freed@gmail.com>
2017-09-15 11:36:11 -0700

    convert to stateful class-based component

--- src/components/Buddy.jsx
+++ src/components/Buddy.jsx
@@ -3,13 +3,20 @@ import PropTypes from "prop-types";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 
-function Buddy(props) {
-  return (
-    <div>
-      <NewPlantForm createPlant={this.props.addPlant}/>
-      <PlantList/>
-    </div>
-  );
+class Buddy extends React.Component {
+
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <NewPlantForm createPlant={this.props.addPlant}/>
+        <PlantList/>
+      </div>
+    );
+  }
 }
 
 Buddy.propTypes = {
