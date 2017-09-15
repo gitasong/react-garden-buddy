f066e671ece244068a40fa528429442fa9d39296
Nicole Freed <nicole.freed@gmail.com>
2017-09-15 11:07:29 -0700

    add refs

--- src/components/NewPlantForm.jsx
+++ src/components/NewPlantForm.jsx
@@ -13,30 +13,35 @@ class NewPlantForm extends React.Component {
         <form>
           <label>Common Name: </label>
           <input
+            ref="_name"
             type="text"
             id="name"
             placeholder="Venus Fly Trap"/>
           <br/>
           <label>Image URL: </label>
           <input
+            ref="_image"
             type="text"
             id="image"
             placeholder="Image URL"/>
           <br/>
           <label>Species/Botanical Name: </label>
           <input
+            ref="_species"
             type="text"
             id="species"
             placeholder="Paeonia Lactiflora"/>
           <br/>
           <label>Category: </label>
           <input
+            ref="_category"
             type="text"
             id="category"
             placeholder="flowering plant"/>
           <br/>
           <label>Frequency (Days Between Watering): </label>
           <input
+            ref="_frequency"
             type="number"
             id="frequency"
             placeholder="1, 3, 7, etc."/>
