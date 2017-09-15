42a917fbc04f695390c65ecfa0b370e60b9487cd
Nicole Freed <nicole.freed@gmail.com>
2017-09-15 10:32:08 -0700

    create Buddy component; import and add NewPlantForm component/element

---
+++ src/components/Buddy.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NewPlantForm from "./NewPlantForm";
+
+function Buddy(props) {
+  return (
+    <div>
+      <NewPlantForm/>
+    </div>
+  );
+}
+
+export default Buddy;
