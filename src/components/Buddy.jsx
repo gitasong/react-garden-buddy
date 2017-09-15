a92bd8c3257c6a87a6f3267bc3260c8fa8172635
Nicole Freed <nicole.freed@gmail.com>
2017-09-15 10:45:58 -0700

    move PlantList to Buddy from App

--- src/components/Buddy.jsx
+++ src/components/Buddy.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NewPlantForm from "./NewPlantForm";
+import PlantList from "./PlantList";
 
 function Buddy(props) {
   return (
     <div>
       <NewPlantForm/>
+      <PlantList/>
     </div>
   );
 }
