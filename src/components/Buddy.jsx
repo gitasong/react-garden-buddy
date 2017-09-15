c4cd2c8f5e2361ef92dfd65c44813ef4c66eb614
Nicole Freed <nicole.freed@gmail.com>
2017-09-15 11:30:34 -0700

    pass createPlant as addPlant

--- src/components/Buddy.jsx
+++ src/components/Buddy.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 
 function Buddy(props) {
   return (
     <div>
-      <NewPlantForm/>
+      <NewPlantForm createPlant={this.props.addPlant}/>
       <PlantList/>
     </div>
   );
 }
+
+Buddy.propTypes = {
+  createPlant: PropTypes.func
+};
 
 export default Buddy;
