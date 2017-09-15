cafd908e60370fdcc37dd1ee24493f66afe72958
Nicole Freed <nicole.freed@gmail.com>
2017-09-15 11:05:46 -0700

    convert NewPlantForm to class-based component

--- src/components/NewPlantForm.jsx
+++ src/components/NewPlantForm.jsx
@@ -1,44 +1,51 @@
 import React from "react";
 
-function NewPlantForm(props) {
-  return (
-    <div>
-      <h2>Add A Plant</h2>
-      <form>
-        <label>Common Name: </label>
-        <input
-          type="text"
-          id="name"
-          placeholder="Venus Fly Trap"/>
-        <br/>
-        <label>Image URL: </label>
-        <input
-          type="text"
-          id="image"
-          placeholder="Image URL"/>
-        <br/>
-        <label>Species/Botanical Name: </label>
-        <input
-          type="text"
-          id="species"
-          placeholder="Paeonia Lactiflora"/>
-        <br/>
-        <label>Category: </label>
-        <input
-          type="text"
-          id="category"
-          placeholder="flowering plant"/>
-        <br/>
-        <label>Frequency (Days Between Watering): </label>
-        <input
-          type="number"
-          id="frequency"
-          placeholder="1, 3, 7, etc."/>
-        <br/>
-        <button type="submit">Add Plant</button>
-      </form>
-    </div>
-  );
+class NewPlantForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Add A Plant</h2>
+        <form>
+          <label>Common Name: </label>
+          <input
+            type="text"
+            id="name"
+            placeholder="Venus Fly Trap"/>
+          <br/>
+          <label>Image URL: </label>
+          <input
+            type="text"
+            id="image"
+            placeholder="Image URL"/>
+          <br/>
+          <label>Species/Botanical Name: </label>
+          <input
+            type="text"
+            id="species"
+            placeholder="Paeonia Lactiflora"/>
+          <br/>
+          <label>Category: </label>
+          <input
+            type="text"
+            id="category"
+            placeholder="flowering plant"/>
+          <br/>
+          <label>Frequency (Days Between Watering): </label>
+          <input
+            type="number"
+            id="frequency"
+            placeholder="1, 3, 7, etc."/>
+          <br/>
+          <button type="submit">Add Plant</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default NewPlantForm;
