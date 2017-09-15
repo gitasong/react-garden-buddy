c0751ae6f85e7e100d79ae921e2fbfd7c8efa9b4
Nicole Freed <nicole.freed@gmail.com>
2017-09-15 15:07:59 -0700

    create WaterButton component w/PropTypes

---
+++ src/components/WaterButton.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function WaterButton(props) {
+  return (
+    <div>
+      <button>Water</button>
+    </div>
+  );
+}
+
+WaterButton.propTypes = {
+
+};
+
+export default WaterButton;
