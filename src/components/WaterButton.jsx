c4b61973576b38ee076aef813ad6301c0b8bc943
Nicole Freed <nicole.freed@gmail.com>
2017-09-18 08:48:24 -0700

    create WaterButton component w/PropTypes

--- src/components/WaterButton.jsx
+++ src/components/WaterButton.jsx
@@ -4,13 +4,13 @@ import PropTypes from "prop-types";
 function WaterButton(props) {
   return (
     <div>
-      <button>Water</button>
+      <button>Water Plant</button>
     </div>
   );
 }
 
 WaterButton.propTypes = {
-
+  
 };
 
 export default WaterButton;
