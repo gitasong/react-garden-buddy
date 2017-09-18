2f5d1185e421c028e5b05359b94bc4e29b9d5210
Nicole Freed <nicole.freed@gmail.com>
2017-09-18 08:58:11 -0700

    add dummy onClick function to button

--- src/components/WaterButton.jsx
+++ src/components/WaterButton.jsx
@@ -4,13 +4,13 @@ import PropTypes from "prop-types";
 function WaterButton(props) {
   return (
     <div>
-      <button>Water Plant</button>
+      <button onClick={() => {sayHello()}}>Water Plant</button>
     </div>
   );
 }
 
 WaterButton.propTypes = {
-  
+  sayHello: PropTypes.func,
 };
 
 export default WaterButton;
